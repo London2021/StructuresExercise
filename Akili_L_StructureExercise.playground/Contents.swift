eeb4b33c4e3f56eaa923212ee6fbe16f97ce8156
London2021 <67976260+London2021@users.noreply.github.com>
2020-07-28 13:21:50 -0500

    Structures Commit Exercise
    Structures Commit Exercise

---
+++ Akili_L_StructureExercise.playground/Contents.swift
@@ -0,0 +1,26 @@
+import Cocoa
+
+struct engines {
+    var engine1:String
+    var engine2:String
+    var engine3:String
+    var engine4:String
+    var engine5:String
+    var engine6: String
+    init() {
+        engine1 = "vee"
+        engine2 = "inline"
+        engine3 = "straight"
+        engine4 = "vr "
+        engine5 = "boxer"
+        engine6 = "rotary"
+    }
+    
+
+
+var engine01 = engines()
+var engine02 = engines ()
+
+print("engine1 is \(engine01.engine1)")
+//print("engine2 is \(myengines.engine2)")
+//print("engine3 is \(myengines.engine3)")
